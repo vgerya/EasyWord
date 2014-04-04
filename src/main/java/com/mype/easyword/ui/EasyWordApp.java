8dd3d281eaee318f98c16051f3aab170409510f1
Vitaliy Gerya <vgerya@skytide.com>
2014-04-04 17:02:45 +0300

    Chaged logger to logback. Added resource bundle for text on forms. Added CSS styles.

--- src/main/java/com/mype/easyword/ui/EasyWordApp.java
+++ src/main/java/com/mype/easyword/ui/EasyWordApp.java
@@ -6,15 +6,24 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 /**
  * @author Vitaliy Gerya
  */
 public class EasyWordApp extends Application {
     @Override
     public void start(final Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("vocabulary/vocabulary.fxml"));
-        stage.setTitle("EasyWord");
-        stage.setScene(new Scene(root, 800, 600));
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setResources(ResourceBundle.getBundle("bundles.messages", Locale.getDefault()));
+        Parent root = fxmlLoader.load(getClass().getResource("vocabulary/vocabulary.fxml"));
+
+        stage.setTitle(fxmlLoader.getResources().getString("vocabularyApp.title"));
+        Scene scene = new Scene(root, 800, 600);
+        scene.getStylesheets().add("/styles/styles.css");
+        stage.setScene(scene);
+
         stage.show();
     }
 
--- src/main/java/com/mype/easyword/ui/vocabulary/VocabularyController.java
+++ src/main/java/com/mype/easyword/ui/vocabulary/VocabularyController.java
@@ -3,17 +3,27 @@ package com.mype.easyword.ui.vocabulary;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MultipleSelectionModel;
 
+import java.net.URL;
 import java.time.LocalDateTime;
+import java.util.ResourceBundle;
 
 /**
  * @author Vitaliy Gerya
  */
-public class VocabularyController {
+public class VocabularyController implements Initializable {
+    private ResourceBundle resources;
     @FXML
     private ListView<VocabularyRow> vocabularyListView;
+
+
+    @Override
+    public void initialize(final URL location, final ResourceBundle resources) {
+        this.resources = resources;
+    }
 
     @FXML
     public void addVocabulary(javafx.event.ActionEvent actionEvent) {
@@ -39,4 +49,5 @@ public class VocabularyController {
     public void removeWord(ActionEvent actionEvent) {
 
     }
+
 }
